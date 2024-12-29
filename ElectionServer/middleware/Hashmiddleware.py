f5cb9f079fdaa4e3add1369d8743604f420b4f11
Nikita <143941740+Nickseqin2123@users.noreply.github.com>
2024-12-29 14:47:20 +0300

    Update Hashmiddleware.py

--- ElectionServer/middleware/Hashmiddleware.py
+++ ElectionServer/middleware/Hashmiddleware.py
@@ -1,1 +1,17 @@
+from django.http import HttpResponse, HttpRequest, JsonResponse
 
+
+class HashMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request: HttpRequest):
+        response: HttpResponse = self.get_response(request)
+        
+        hash_header = request.headers.get('X-Hash')
+        
+        # if hash_header == '':
+        #     return response
+        
+        # return JsonResponse({'response': 'Тут ничего интересного. Иди отсюда, путник'}, status=403)
+        return response
