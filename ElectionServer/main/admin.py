0861f70f0aba1321febc9604b5f3ac0c77bb77f1
Nickseqin2123 <bletigri@gmail.com>
2025-04-30 10:29:53 +0300

    srochni dodep

--- ElectionServer/main/admin.py
+++ ElectionServer/main/admin.py
@@ -5,16 +5,16 @@ from .models import UserModel
 @admin.register(UserModel)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id_card', 'election', 'choice')
-    # readonly_fields = ('id_card', 'election', 'choice')
+    readonly_fields = ('id_card', 'election', 'choice')
 
-    # def has_add_permission(self, request):
-    #     return False  # Запрет на добавление
+    def has_add_permission(self, request):
+        return False  # Запрет на добавление
 
-    # def has_change_permission(self, request, obj=None):
-    #     return False  # Запрет на изменение
+    def has_change_permission(self, request, obj=None):
+        return False  # Запрет на изменение
 
-    # def has_delete_permission(self, request, obj=None):
-    #     return False  # Запрет на удаление
+    def has_delete_permission(self, request, obj=None):
+        return False  # Запрет на удаление
 
-    # def has_view_permission(self, request, obj=None):
-    #     return True  # Разрешить только просмотр (опционально)
+    def has_view_permission(self, request, obj=None):
+        return True  # Разрешить только просмотр (опционально)
