3a7af57644dc4c352cb7d75bc9732bc27addd24c
Nikita <bletigri@gmail.com>
2025-08-04 15:41:52 +0300

    fix tests

--- ElectionServer/entrypoint.sh
+++ ElectionServer/entrypoint.sh
@@ -3,13 +3,19 @@ set -e
 
 echo "Ожидание запуска Postgres"
 
-until pg_isready -h $POSTGRES_HOST -d $POSTGRES_DB -U $POSTGRES_USER; do
+until pg_isready -h "$POSTGRES_HOST" -d "$POSTGRES_DB" -U "$POSTGRES_USER"; do
   sleep 5
 done
 
 echo "Postgres запущена. Выполнение миграций..."
 python manage.py makemigrations
 python manage.py migrate
+
+if [ "$TEST" = "1" ]; then
+  echo "Запуск тестов..."
+  python manage.py test
+  exit 0
+fi
 
 WORKERS=$(( $(nproc) * 2 + 1 ))
 echo "Запуск Gunicorn с $WORKERS воркерами..."
